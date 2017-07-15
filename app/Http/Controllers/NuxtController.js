2278a76e6c7b63961d25f6d3a90b4ab6b70ded16
Emmanuel Béziat <contact@emmanuelbeziat.com>
2017-07-15 13:05:41 +0200

    make it work fix

--- app/Http/Controllers/NuxtController.js
+++ app/Http/Controllers/NuxtController.js
@@ -10,12 +10,10 @@ class NuxtController {
 		let config = Config.get('nuxt')
 		config.dev = Env.get('NODE_ENV') === 'development'
 		this.nuxt = new Nuxt(config)
-			.then((nuxt) => {
-				this.nuxt = nuxt
-				if (config.dev) {
-					this.nuxt.build()
-				}
-			})
+
+		if (config.dev) {
+			this.nuxt.build()
+		}
 	}
 
 	* render(request, response) {
